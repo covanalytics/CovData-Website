24cf51356a6b836766ec9ba706ea56c4536006b3
covanalytics <tsink@covingtonky.gov>
2022-06-26 16:18:16 -0400

    sample of data js structure

---
+++ repository/objects.js
@@ -0,0 +1,22 @@
+{
+  "data": [
+     {
+      "dataset": "Police Responses",
+      "year": "2018",
+      "department": "Police",
+      "division": "Police",
+	    "description":"Police runs are the events captured in an agency's Computer-Aided Dispatch (CAD) system used to facilitate police response. This dataset includes data on the type, location, and priority of calls dispatched for police service. This data is updated monthly. During the first week of the month, new data is posted for the previous month. <p><p><b>DISCALIMER:</b> In compliance with privacy laws, all public safety datasets are anonymized and appropriately redacted prior to publication on the City of Covington's Open Data Portal. This means that all personally identifiable information has been removed.</p></p>",
+	    "format":	"Datasets",
+       "download": "CSV",
+	    "url": "http://bit.ly/PoliceRuns2018_csv",
+	    "urlOriginal": "https://drive.google.com/uc?export=download&id=10Ppmi-L4r84_-2hU9TqsLDhVWogymQUz",
+	    "reportFormat": "PDF",
+	    "reportURL": "http://bit.ly/PoliceRespDS",
+	    "reportOriginal": "https://drive.google.com/uc?export=download&id=0BxZjyP_Xfv7ceU1kRUpyZ0dWNHM", 
+	    "reportInfo": "This report provides the official summary context for this COVDATA dataset.  For an interactive visualization of this dataset, please check out our <a class='dashboard' href='http://data.covingtonky.gov/Dashboards/PublicSafety/PoliceRuns/'>dashboard.</a>"
+    },
+    { NewDataSet:
+      
+    },
+  ]
+}
